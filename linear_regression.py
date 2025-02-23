f4b03eb37ce302d19d8379579237ef40782930fe
Kadir FERİK <100694700+kadirferik@users.noreply.github.com>
2025-02-23 21:10:16 +0300

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -4,6 +4,7 @@ Created on Tue Jan 14 22:56:46 2025
 @author: kadirferik
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 def LinearRegression(x, y):
     x = np.array(x)
@@ -37,3 +38,16 @@ def StandardError(y_pred, y):
         raise ValueError("Sample size must be greater than 2.")
     
     return standard_error
+
+x = np.linspace(0, 10, 100) + np.random.normal(0, 0.5, 100)
+y = 2 * x + 1 + np.random.normal(0, 1.5, 100)
+
+a, b = LinearRegression(x, y)
+y_pred = a * x + b
+standard_error = StandardError(y_pred, y)
+
+plt.scatter(x, y)
+plt.plot(x, y_pred, color="red")
+plt.show()
+
+print(standard_error)
